d7ad902aaef980120ccd7eb54cd4dcb04839b4d3
GoastcraftHD <simon.ickler@gmail.com>
2025-05-22 19:47:36 +0200

    Added custom type definitions

---
+++ src/common/Types.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef unsigned char U8;
+typedef unsigned short int U16;
+typedef unsigned int U32;
+typedef unsigned long int U64;
+
+typedef char I8;
+typedef short int I16;
+typedef int I32;
+typedef long int I64;
+
+typedef float F32;
+typedef double F64;
